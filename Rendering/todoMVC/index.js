ef59bd4681887001e70f0db45e91ffd5b9264325
youss95 <usekim7555@gmail.com>
2022-07-23 21:13:01 +0900

    refactor: view 렌더링 함수 분리

--- Rendering/todoMVC/index.js
+++ Rendering/todoMVC/index.js
@@ -1,18 +1,14 @@
 import getTodos from "./getTodos.js";
-import view from "./view.js";
+import appView from "./view/app.js";
+
 const state = {
   todos: getTodos(),
   currentFilter: "All",
 };
 
 const main = document.querySelector(".todoapp");
-console.log(main);
-/*
- * main : target element -> cloneNode로 복제
- * state : 요소를 바꿀 state
- */
+
 window.requestAnimationFrame(() => {
-  const newMain = view(main, state);
-  console.log(newMain);
+  const newMain = appView(main, state);
   main.replaceWith(newMain);
 });
